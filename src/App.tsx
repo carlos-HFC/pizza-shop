adfe9e5fa2bb7245639002ed001215efe2dcdeec
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-25 13:39:22 -0300

    feat: add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Helmet, HelmetProvider } from "react-helmet-async"
 import { RouterProvider } from "react-router-dom"
 import { Toaster } from "sonner"
 
+import { ThemeProvider } from "./components/theme/theme-provider"
+
 import { router } from "./pages/routes"
 
 import "./global.css"
@@ -9,9 +11,11 @@ import "./global.css"
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
-      <Toaster richColors />
+      <ThemeProvider storageKey="@pizza-shop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { HomeIcon, PizzaIcon, UtensilsCrossedIcon } from "lucide-react"
 
 import { NavLink } from "./nav-link"
+import { ThemeToggle } from "./theme/theme-toggle"
 import { Separator } from "./ui/separator"
 
 export function Header() {
@@ -24,6 +25,10 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   )
