2e8105d37c0da9a791aa2138551f2fa0d1b56a57
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-25 13:35:22 -0300

    feat: add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,30 @@
+import { HomeIcon, PizzaIcon, UtensilsCrossedIcon } from "lucide-react"
+
+import { NavLink } from "./nav-link"
+import { Separator } from "./ui/separator"
+
+export function Header() {
+  return (
+    <header className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <PizzaIcon className="size-6" />
+
+        <Separator
+          orientation="vertical"
+          className="h-6"
+        />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to="/">
+            <HomeIcon className="size-4" />
+            Início
+          </NavLink>
+          <NavLink to="/orders">
+            <UtensilsCrossedIcon className="size-4" />
+            Pedidos
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  )
+}
---
+++ src/components/nav-link.tsx
@@ -0,0 +1,10 @@
+import { NavLink as Nav, NavLinkProps } from "react-router-dom"
+
+export function NavLink(props: NavLinkProps) {
+  return (
+    <Nav
+      {...props}
+      className="flex items-center gap-1.5 text-sm font-medium text-muted-foreground hover:text-foreground aria-[current=page]:text-foreground"
+    />
+  )
+}
