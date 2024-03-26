f91ea9d3fc6601fb1f663f2631171160b3af1fe8
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-26 09:37:10 -0300

    feat: add axios, env validation and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
+import "./global.css"
+
+import { QueryClientProvider } from "@tanstack/react-query"
 import { Helmet, HelmetProvider } from "react-helmet-async"
 import { RouterProvider } from "react-router-dom"
 import { Toaster } from "sonner"
 
 import { ThemeProvider } from "./components/theme/theme-provider"
-
+import { queryClient } from "./lib/react-query"
 import { router } from "./pages/routes"
-
-import "./global.css"
 
 export function App() {
   return (
     <HelmetProvider>
       <ThemeProvider storageKey="@pizza-shop-theme">
         <Helmet titleTemplate="%s | pizza.shop" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
         <Toaster richColors />
       </ThemeProvider>
     </HelmetProvider>
