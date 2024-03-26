1a9025782acc04956f525e3d1055ec0a825dcc00
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-26 11:22:38 -0300

    feat: list orders

---
+++ src/components/order-status.tsx
@@ -0,0 +1,39 @@
+import clsx from "clsx"
+
+type OrderStatus =
+  | "pending"
+  | "canceled"
+  | "processing"
+  | "delivering"
+  | "delivered"
+
+interface OrderStatusProps {
+  status: OrderStatus
+}
+
+const orderStatusMap: Record<OrderStatus, string> = {
+  canceled: "Cancelado",
+  delivered: "Entregue",
+  delivering: "Em entrega",
+  pending: "Pendente",
+  processing: "Em preparo",
+}
+
+export function OrderStatus({ status }: OrderStatusProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <span
+        className={clsx(
+          "size-2 rounded-full",
+          status === "canceled" && "bg-rose-500",
+          status === "pending" && "bg-slate-400",
+          status === "delivered" && "bg-emerald-500",
+          ["processing", "delivering"].includes(status) && "bg-amber-500",
+        )}
+      />
+      <span className="font-medium text-muted-foreground">
+        {orderStatusMap[status]}
+      </span>
+    </div>
+  )
+}
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,8 +1,7 @@
+import { useQuery } from "@tanstack/react-query"
 import { Helmet } from "react-helmet-async"
 
-import { OrderTableFilters } from "./order-table-filters"
-import { OrderTableRow } from "./order-table-row"
-
+import { getOrders } from "@/api/get-orders"
 import { Pagination } from "@/components/pagination"
 import {
   Table,
@@ -12,7 +11,15 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
+import { OrderTableFilters } from "./order-table-filters"
+import { OrderTableRow } from "./order-table-row"
+
 export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ["orders"],
+    queryFn: getOrders,
+  })
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -39,8 +46,11 @@ export function Orders() {
               </TableHeader>
 
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => (
-                  <OrderTableRow key={i} />
+                {result?.orders?.map(order => (
+                  <OrderTableRow
+                    key={order.orderId}
+                    order={order}
+                  />
                 ))}
               </TableBody>
             </Table>
